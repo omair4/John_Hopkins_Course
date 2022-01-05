8d2be1703f466809a1b3fdffd358402068f73443
omair4 <90240473+omair4@users.noreply.github.com>
2022-01-06 01:14:07 +0500

    initial commit

---
+++ Jhu_course_mod2_solution/README.md
@@ -0,0 +1,2 @@
+# Jhu_course_mod2_solution
+Solution for module 2 assignment for JHU course
---
+++ Jhu_course_module4_solution/SpeakGoodBye.js
@@ -0,0 +1,11 @@
+(function(window) {
+
+var byeSpeaker = {};
+
+var speakWord = "Good Bye";
+
+byeSpeaker.SpeakGoodbye=function speak(name) {
+  console.log(speakWord + " " + name);
+}
+window.byeSpeaker = byeSpeaker;
+})(window);
